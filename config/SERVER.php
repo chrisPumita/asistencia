f6e8bad8786304d6222c4b092f80df8fbb8ac3cf
Christan RCSG <christian.floppy@gmail.com>
2022-04-12 15:04:19 -0500

    Estructura Inicial de Proyecto

---
+++ config/SERVER.php
@@ -0,0 +1,26 @@
+<?php
+#CONFIGURACION DEL SERVIDOR
+#cONSTANTES DE PARAMETROS DE LA CONEXION A LA BD
+#
+date_default_timezone_set("America/Mexico_City");
+
+const SERVER = "localhost";
+const DB = "DB_NAME";
+const USER = "root";
+const PASS = "";
+
+#CONEXION POR PDO en caso de usar PDO
+
+const SGDB = "mysql:host=".SERVER.";dbname=".DB;
+
+#Encriptar por hash
+const METHOD = "AES-256-CBC";
+const SECRET_KEY = '$CHRISRSCG@2022';
+const SECRET_IV = '037970';
+
+#USER MAILING
+const HOST = '';
+const NAME_MAIL = "";
+const USERNAME_MAIL = '';
+const PW_MAIL = '';
+const PORT_MAIL = 465;
