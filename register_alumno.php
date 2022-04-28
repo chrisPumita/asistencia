334ad68b96b2d7815c8fe144b29e91e35a6f6da4
ZSSH7 <zssh_7@comunidad.unam.mx>
2022-04-28 18:18:23 -0500

    registro alumno

--- register_alumno.php
+++ register_alumno.php
@@ -20,58 +20,89 @@ $path = "./"
                 </div>
                 <div class="card shadow-lg">
                     <div class="card-body p-5">
-                        <h1 class="fs-4 card-title fw-bold mb-4 text-center">Iniciar Sesión</h1>
-                        <form method="POST" class="needs-validation" novalidate="" autocomplete="off">
+                        <h1 class="fs-4 card-title fw-bold mb-4 text-center" style="color: darkblue;">Registrarme como Alumno</h1>
+                        <form id="frm-new-profesor" class="needs-validation" novalidate="" autocomplete="off">
+                            <div id="resp"></div>
                             <div class="mb-3">
-                                <label class="mb-2 text-muted" for="name">Nombre</label>
-                                <input id="name" type="text" class="form-control" name="name" value="" required autofocus>
+                                <label class="mb-2 text-muted" for="nombre">Nombre</label>
+                                <input id="nombre" type="text" class="form-control" name="nombre" value="" required autofocus>
                             </div>
                             <div class="mb-3">
-                                <label class="mb-2 text-muted" for="name">Primer Apellido</label>
-                                <input id="name" type="text" class="form-control" name="name" value="" required autofocus>
+                                <label class="mb-2 text-muted" for="app">Primer Apellido</label>
+                                <input id="app" type="text" class="form-control" name="app" value="" required autofocus>
                             </div>
                             <div class="mb-3">
-                                <label class="mb-2 text-muted" for="name">Segundo Apellido</label>
-                                <input id="name" type="text" class="form-control" name="name" value="" required autofocus>
+                                <label class="mb-2 text-muted" for="apm">Segundo Apellido</label>
+                                <input id="apm" type="text" class="form-control" name="apm" value="" required autofocus>
+                            </div>
+
+                            <div class="row">
+                                <div class="col">
+                                    <div class="mb-3">
+                                        <label class="mb-2 text-muted" for="sexo">Sexo</label>
+                                        <select class="form-select" aria-label=".form-select-sm example" name="sexo" id="sexo">
+                                            <option value="0">Hombre</option>
+                                            <option value="1">Mujer</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                     <div class="mb-3">
+                                        <label class="mb-2 text-muted" for="user_name">Usuario</label>
+                                        <input id="user_name" type="text" class="form-control" name="user_name" value="" required autofocus>
+                                    </div>
+                                </div>
                             </div>
                             <div class="mb-3">
-                                <label class="mb-2 text-muted" for="email">E-Mail Address</label>
+                                <label class="mb-2 text-muted" for="apm">No. Cuenta/Matricula</label>
+                                <input id="apm" type="text" class="form-control" name="apm" value="" required autofocus>
+                            </div>                         
+                            <div class="mb-3">
+                                <label class="mb-2 text-muted" for="email">Correo electrónico</label>
                                 <input id="email" type="email" class="form-control" name="email" value="" required>
                                 <div class="invalid-feedback">
-                                    Email is invalid
+                                    Correo electrónico no válido.
                                 </div>
                             </div>
 
-                            <div class="mb-3">
-                                <label class="mb-2 text-muted" for="password">Password</label>
-                                <input id="password" type="password" class="form-control" name="password" required>
-                                <div class="invalid-feedback">
-                                    Password is required
+                             <div class="row">
+                                <div class="col">
+                                    <div class="mb-3">
+                                        <label class="mb-2 text-muted" for="pwd">Contraseña</label>
+                                        <input id="pwd" type="password" class="form-control" name="pwd" required>
+                                        <div class="invalid-feedback">
+                                            La contraseña es requerida
+                                        </div>
+                                    </div>
                                 </div>
+                                <div class="col">
+                                     <div class="mb-3">
+                                        <label class="mb-2 text-muted" for="pwd_confirm">Repetir contraseña</label>
+                                        <input id="pwd_confirm" type="password" class="form-control" name="pwd_confirm" required>
+                                        <div class="invalid-feedback">
+                                            La contraseña es requerida
+                                        </div>
+                                    </div>
+                                </div>
+                                
                             </div>
-
                             <p class="form-text text-muted mb-3">
                                 <input class="form-check-input" type="checkbox" value="" id="chkTerms">
                                 <label class="form-check-label" for="chkTerms">
                                     He leido los terminos y condiciones
                                 </label>
                             </p>
-
-                            <div class="align-items-center d-flex">
+                            <div class="" style="text-align: center;">
                                 <button type="submit" class="btn btn-primary ms-auto">
-                                    Registrar
+                                    Crear Cuenta
                                 </button>
                             </div>
                         </form>
                     </div>
-                    <div class="card-footer py-3 border-0">
-                        <div class="text-center">
-                            ¿Ya tienes una cuenta? <a href="./login.php" class="text-dark">Iniciar Sesión</a>
-                        </div>
-                    </div>
+                    
                 </div>
                 <div class="text-center mt-5 text-muted">
-                    Copyright PapeAndHome © 2022. All rights reserved.
+                    Copyright oKList © 2022. All rights reserved.
                 </div>
             </div>
         </div>
@@ -81,6 +112,6 @@ $path = "./"
 
 <?php include $path."includes_general/js.php"?>
 
-<script src="services/template.js"></script>
+<script src="services/registro.js"></script>
 <!-- Initialize Swiper -->
 </html>
